dec67c2fc15f973c7b2e4aa79fa1d29ada7290d1
dodevs <douglass.sousa@outlook.com.br>
2018-03-02 19:19:00 -0300

    Exercicio 4 corrigido

--- revisao/exercicio4.py
+++ revisao/exercicio4.py
@@ -1,16 +1,16 @@
-def composta(texto, i):
-
-
 def separaPal(texto):
-    #separador = [" ", ".", ",", "!", "?"]
     listaPal = []
     temp = ""
-    for i in range(len(texto)):#Eu comi um pe-de-moleque
-        if texto[i].isalnum() || texto[i] == '-' && composta(texto, i):
+    for i in range(len(texto)):#pe-de-moleque
+        if texto[i].isalnum():
             temp += texto[i]
-        elif temp != '':
-            listaPal.append(temp)
-            temp = ""
+        elif len(temp) > 0:
+            if texto[i] == "-":
+                temp += texto[i]
+            else:
+                listaPal.append(temp)
+                temp = ""
+
     if len(temp) > 0:
         listaPal.append(temp)
 
@@ -19,7 +19,7 @@ def separaPal(texto):
 def main():
     texto = 'Eu comi um pe-de-moleque'
     #texto = open('biblia-em-txt.txt','rt').read()
-    print(len(separaPal(texto)))
+    print(separaPal(texto))
 
 if __name__ == "__main__":
     main()
