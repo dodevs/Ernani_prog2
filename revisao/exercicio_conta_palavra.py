f7547909f9625a6d4dfda1d9c89ee87b19c34d35
dodevs <dev08!hi!>
2018-03-12 22:33:39 -0300

    Exercicio para proxima aula

---
+++ revisao/exercicio_conta_palavra.py
@@ -0,0 +1,41 @@
+def getIndex(conj, elem):
+    for i in range(len(conj)):
+        if conj[i] == elem:
+            return i
+
+def separaPal(texto):
+    listaPal = []
+    listaPalCont = []
+    temp = ""
+    for i in range(len(texto)):
+        if texto[i].isalnum():
+            temp += texto[i]
+        elif len(temp) > 0:
+            if texto[i] == "-":
+                temp += texto[i]
+            else:
+                if temp not in listaPal:
+                    listaPal.append(temp)
+                    listaPalCont.append(1)
+                else:
+                    listaPalCont[getIndex(listaPal, temp)] += 1
+                temp = ""
+
+    if len(temp) > 0:
+        if temp not in listaPal:
+            listaPal.append(temp)
+            listaPalCont.append(1)
+        else:
+            listaPalCont[getIndex(listaPal, temp)] += 1
+
+    return listaPal, listaPalCont
+
+def main():
+    texto = 'O rato o tres'
+    #texto = open('biblia-em-txt.txt','rt').read()
+    result = (separaPal(texto))
+    for i in range(len(result[0])):
+        print(result[0][i],"|",result[1][i])
+
+if __name__ == "__main__":
+    main()
