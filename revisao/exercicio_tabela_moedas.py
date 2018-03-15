a604716d557c04d530b4334834ec767fcf9ec757
dodevs <dev08!hi!>
2018-03-15 08:05:45 -0300

    Implementada ideia

--- revisao/exercicio_tabela_moedas.py
+++ revisao/exercicio_tabela_moedas.py
@@ -1,4 +1,4 @@
-from random import random
+from random import uniform
 
 def tabelaM():
     moedas = ["R$","US$","Fr","Rp","Pe$","Mr"]
@@ -7,9 +7,11 @@ def tabelaM():
         for j in range(len(moedas)):
             if moedas[i] == moedas[j]:
                 tabelaMon[(moedas[i], moedas[j])] = 1
-            else:
-                taxaCV = float(input("Taxa de conversao {} para {}: ".format(moedas[i], moedas[j])))
+            elif (moedas[i], moedas[j]) not in tabelaMon:
+                taxaCV = uniform(1.5, 8.90)
+                #taxaCV = float(input("Taxa de conversao {} para {}: ".format(moedas[i], moedas[j])))
                 tabelaMon[(moedas[i], moedas[j])] = taxaCV
+                tabelaMon[(moedas[j], moedas[i])] = taxaCV ** -1
 
     return tabelaMon
 
