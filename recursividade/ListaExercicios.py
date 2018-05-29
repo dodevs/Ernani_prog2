e66f688a702a1a52f31d919fd938042f9d5a6c3e
dodevs <douglass.sousa@outlook.com.br>
2018-05-29 11:02:31 -0300

    Raiz

--- recursividade/ListaExercicios.py
+++ recursividade/ListaExercicios.py
@@ -19,13 +19,37 @@ def divisaoRecursiva(x,y):#10,2
         return divisaoRecursiva(subt,y)
 
 #Raiz com método newthon raphson
-def raizRecursiva():
-    return 0
+#Xi = (Xo² + N) / 2 * Xo
+#N := 27
+#Numero elevado ao quadrado mais proximo de 27: 5 = 5² = 25
+#Xo := 5
+#Xi = (25 + 27) / 2 * 5
+#Xi := 5,2
+
+#Agora o Xi se torna o Xo da proxima aproximação
+#Xi = (5,2² + 27) / 2 * 5,2
+def raizRecursiva(n,t,xo=0):
+    if t == 1:
+        Xo = [x for x in range(n)  if (x**2) < n ][-1]
+    else:
+        Xo = xo
+    if n == 1:
+        return 1
+    elif n == 0:
+        return 0
+    else:
+        Xi = ((Xo ** 2) + n) / (2 * Xo)
+        precisao = Xo - Xi
+        while i < t
+            
+
+        return Xi
 
 def main():
-    print(somaRecursiva([1,2,3]))
-    print(produtoRecursivo(3,3))
-    print(divisaoRecursiva(50,20))
+    #print(somaRecursiva([1,2,3]))
+    #print(produtoRecursivo(3,3))
+    #print(divisaoRecursiva(50,20))
+    print(raizRecursiva(27,1))
 
 
 if __name__ == "__main__":
