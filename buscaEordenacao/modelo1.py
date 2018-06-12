5d7305182eed509ed74ebe3f47a4e0de734481fd
dodevs <dodev@dodev.local>
2018-06-12 08:15:41 -0400

    Introducao a busca e ordenacao

---
+++ buscaEordenacao/modelo1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  modelo1.py
+#  
+#  Copyright 2016 Ernani Leite Ribeiro Filho <ernani@ubuntu>
+#  
+
+def main():
+	lstBDCEP = [['01029901','Rua Florêncio de Abreu 157','Rua','Edifício São Bento','9668','25279'], \
+				['01227200','Angélica','Avenida','- de 1711 ao fim - lado ímpar','9668','25422'],\
+				['01251000','Cardoso de Almeida','Rua','- de 1690 ao fim - lado par','9775','26525'],\
+				['01255010','Antonina','Rua','','9025','26525'],\
+				['01308040','São Miguel','Rua','','9182','25243'],\
+				['01311914','Avenida Paulista 777','Avenida','Edifício Viking','9668','25243'],\
+				['01332906','Alameda Rio Claro 190','Avenida','Edifício Humberto Primo','9025','25243'],\
+				['01416902','Rua da Consolação 3064','Rua','Edifício Jardim América','9668','25280'],\
+				['01426040','das Estrelas Novas','Travessa','','9775','25280'],\
+				['01501001','Doutor João Mendes','Praça','- lado ímpar','9668','25279'],\
+				['01543000','Heitor Peixoto','Rua','- até 429/430','9025','25258'],\
+				['01546120','São João Del Rei','Rua','','9025','25633'],\
+				['02072001','Conceição','Avenida','- de 911/912 a 1573/1574','9668','25270'],\
+				['02084150','Articura','Rua','','9668','26807'],\
+				['02119020','Prefeito Milton Improta','Rua','- até 309/310','9182','26853'],\
+				['02123005','Manuel Lainez','Travessa','','9775','25820'],\
+				['02131040','Hiroshima','Rua','','9025','26851'],\
+				['02138000','Lourenço Billis','Praça','','9668','27006'],\
+				['02140030','Antionha','Rua','','9025','27006'],\
+				['02147015','Cláudio Leonardi','Praça','','9775','26379']]
+				
+	print(lstBDCEP)
+				
+	return 0
+
+if __name__ == '__main__':
+	main()
+
